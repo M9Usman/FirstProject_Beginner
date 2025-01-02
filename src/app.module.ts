aadf0d00a1362f40664476f468d05f23318f89f0
Mo.Usman <musman38302@gmail.com>
2025-01-02 09:09:00 +0500

    'fixed problems'
    eslintrc was giving red lines for format issue.
    and there was an instance of user module importing in app.module.ts which does not exsists so i deleted it.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
